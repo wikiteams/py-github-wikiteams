6b4d8d210b6999d276fa4d73d5f6e0766e48c0b2
WikiTeams <info@wikiteams.pl>
2013-06-17 01:01:52 +0200

    handle repos properly

--- iterate.py
+++ iterate.py
@@ -6,9 +6,13 @@ def handle_row(row):
     print 'inside handle_row(row)'
     node = row[0]
     print node
+    lista = str(node).split("/")
+    print lista
+    print 'len: ' + str(len(lista))
+    handle_repo(lista[3], lista[4].split("\"")[0])
 
-def handle_repo(name,owner):
+def handle_repo(owner,name):
     print 'name: ' + name
     print 'owner: ' + owner
 
-cypher.execute(graph_db, "START z=node(*) RETURN z", row_handler=handle_row)
+cypher.execute(graph_db, "START z=node(*) WHERE id(z) > 0  RETURN z", row_handler=handle_row)
