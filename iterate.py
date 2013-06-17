aab0c98811a593e5e527649f14a2ae356d568ffa
WikiTeams <info@wikiteams.pl>
2013-06-17 20:05:26 +0200

    futher

--- iterate.py
+++ iterate.py
@@ -15,25 +15,43 @@ def handle_row(row):
     print 'inside handle_row(row)'
     node = row[0]
     print node
-    lista = str(node).split("/")
-    print lista
-    print 'len: ' + str(len(lista))
-    handle_repo(lista[3], lista[4].split("\"")[0])
+    #lista = str(node).split("/")
+    #print lista
+    #print 'len: ' + str(len(lista))
+    #handle_repo(lista[3], lista[4].split("\"")[0])
+    #print node['owner'] + " " + node['name']
+    handle_repo(node,node['owner'],node['name'])
 
-def handle_repo(owner,name):
+# function handle_repo
+#
+# node - entity of repository in our neo4j database
+# owner - owner of repository
+# name - name of repository
+#
+def handle_repo(node,owner,name):
     print 'name: ' + name
     print 'owner: ' + owner
     repository = gh.repos.get(user=owner,repo=name)
-    #print repository
+    print repository
     coll = gh.repos.collaborators.list(user=owner,repo=name)
+    watchers = gh.repos.watchers.list(user=owner,repo=name)
+    #watchers_list = list(watchers.get_page(1))
+    watchers_pages = watchers.pages
+    print 'Total pages: ' + str(watchers_pages)
+    node['watchers_pages'] = watchers_pages
+    print 'saved watchers_pages'
+    #watchers_count = len(watchers_list)
     #print coll.all()
     #result = repository.collaborators.list()
     #print result.all()
+    print owner
     for resource in coll.iterator():
-	add_collaborator(resource, repository, owner)
+	#add_collaborator(resource, repository, owner)
+	print resource
 
 def add_collaborator(name,repo,owner):
-    cyper.execute(graph_db, "CREATE (n {name: {" + name + "}}")
+    cyper.execute(graph_db, "CREATE (n {type: user, name: {" + name + "}}")
+    cyper.execute(graph_db, "START n=node(*), m=node(*) WHERE (n.type=user and n.name=" + name + ") and m.")
     print 'adding a collaborator ' + str(name) + ' to repo ' + str(repo)
     
 
